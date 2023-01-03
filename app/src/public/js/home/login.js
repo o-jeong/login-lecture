c9b46c9daa0fe095e64b3e438724c478ef8efcb9
이지정 <jeong@ijijeong-ui-MacBookAir.local>
2023-01-03 22:27:35 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,5 +12,12 @@ function login(){
         id: id.value,
         pw: pw.value
     };
-    console.log(req);
+    // 서버에 값 보내기
+    fetch('/login', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req)
+    });
 }
