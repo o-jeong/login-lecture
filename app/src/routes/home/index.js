345e6cf6065da944b31b7868553f23613aa2ba9e
이지정 <jeong@ijijeong-ui-MacBookAir.local>
2023-01-09 19:45:42 +0900

    회원가입 라우팅 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,7 +8,9 @@ const ctrl = require("./home.ctrl");
 router.get('/', ctrl.output.hello);
 router.get('/login', ctrl.output.login);
 router.get('/register', ctrl.output.register);
+
 router.post('/login', ctrl.process.login)
+router.post('/register', ctrl.process.register);
 
 // 이 파일을 외부에서 사용할 수 있도록 해준다.
 module.exports = router;    // 외부로 내보내기 해줌
