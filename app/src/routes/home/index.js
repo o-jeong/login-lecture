b58288c2c43d3aab22d6ac757863d837ba5b56b9
이지정 <jeong@ijijeong-ui-MacBookAir.local>
2023-01-09 17:08:42 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,7 @@ const ctrl = require("./home.ctrl");
 
 router.get('/', ctrl.output.hello);
 router.get('/login', ctrl.output.login);
+router.get('/register', ctrl.output.register);
 router.post('/login', ctrl.process.login)
 
 // 이 파일을 외부에서 사용할 수 있도록 해준다.
